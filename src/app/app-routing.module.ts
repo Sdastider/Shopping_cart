08674d7b888651dd8724cf48830c472c1712301d
Sdastider <sankhamala2022@gmail.com>
2022-09-05 16:52:37 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:'login', component:LoginComponent },
+   {path:'signup', component:SignupComponent},
+   {path:'forgotpassword',component:ForgotpasswordComponent},
+   {path:'',redirectTo:'/login', pathMatch:'full'},
+   {
+    path:'admin',
+    canActivate : [AuthGuard],
+    loadChildren: () =>
+        import('./modules/admin/admin.module').then((mod) => mod.AdminModule),
+  },  
+   {path:'**', component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AdminComponent } from './components/admin/admin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotpasswordComponent,
+    NotfoundComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
